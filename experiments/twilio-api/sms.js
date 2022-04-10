d0ef0d0a1eb1764cf4a149f70fe2f361089a4254
Daria Kiseleva <daria.kiseleva@mail.mcgill.ca>
2022-04-10 17:05:09 +0000

    Continue SMS experiments

--- experiments/twilio-api/sms.js
+++ experiments/twilio-api/sms.js
@@ -1,44 +1,11 @@
-//how to install all missing dependencies?
 
-//npm install twilio --no-bin-links
-//npm install ngrok -g
-
-//ngrok authtoken 27ajZJD2vezF7f7SB5pujHVnBSl_2QLuc4ysDUPDGQvvxyZ92
-
-// Download the helper library from https://www.twilio.com/docs/node/install
-// Find your Account SID and Auth Token at twilio.com/console
-// and set the environment variables. See http://twil.io/secure
-
-// #---------TWILIO
-
-// # Twilio credentials
-// TWILIO_ACCOUNT_SID: "ACeadf543eb1764173a0fa8466747f885e"
-// TWILIO_AUTH_TOKEN: "fbff4d0231a03b21a4575424599019d0"
-
-// #Twilio key instead of auth tokens, not sure if will use this...
-// TWILIO_SID=SK7ce5ce1b11c54bd90f7c36856ae44cb3
-// TWILIO_SECRET=cLB85XqC3fJNYfEnP0tyfMJN9AMqO7I8
-
-require("dotenv").config(); ///! Unsure how to get it out of there, pasted neutrally
-
-// console.log(process.env.TWILIO_ACCOUNT_SID);
-
+//These variables should be in .env
 const accountSid = "ACeadf543eb1764173a0fa8466747f885e";
-const authToken = "fbff4d0231a03b21a4575424599019d0";
+const authToken = "f7182d8de74854ec880f4d92013df045";
+const adminPhone = "+15145855582";
+
 const client = require('twilio')(accountSid, authToken);
 
-
-
-const sendMessageToClient = () => {
-  client.messages
-    .create({
-      body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-      from: '+18077709123', //process.env.MY_PHONE_NUMBER
-      to: '+15145855582'
-    })
-    .then(message => console.log(message.sid))
-    .catch((err) => console.log(err));
-}
 
 
   //----Receiving requires a server, setting up a test server for now
@@ -50,22 +17,39 @@ const sendMessageToClient = () => {
   const app = express();
   app.use(express.urlencoded({ extended: true }));
 
+
+
   app.get("/", (req, res) => {
     res.send("The index works!");
   });
   
+
+
   app.post('/sms', (req, res) => {
 
-    //console.log(req.body.Body);
-    const numMinutes = Number(req.body.Body);
-    console.log(numMinutes, typeof(numMinutes));
+    if (req.body.From===adminPhone) {
 
+      const numMinutes = Number(req.body.Body);
+      const twiml = new MessagingResponse();
 
-    const twiml = new MessagingResponse();
-    twiml.message('The Robots are coming! Head for the hills!');
-    res.writeHead(200, {'Content-Type': 'text/xml'});
-    res.end(twiml.toString());
-    console.log("It happened!");
+      console.log("Num minutes: " + numMinutes);
+
+      //Block for some valid input
+      if (numMinutes && numMinutes >= 0) {
+
+        //
+
+        
+        twiml.message('Thank you!');
+        res.writeHead(200, {'Content-Type': 'text/xml'});
+        res.end(twiml.toString());
+      } else {
+        twiml.message('Invalid response - try again!');
+        res.writeHead(200, {'Content-Type': 'text/xml'});
+        res.end(twiml.toString());
+      }
+
+    }
 
   });
 
@@ -74,3 +58,4 @@ const sendMessageToClient = () => {
   http.createServer(app).listen(8080, () => {
     console.log('Express server listening on port 8080');
   });
+
