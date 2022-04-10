20724fd183f6fa170e1c2608871ff2093810df91
Daria Kiseleva <daria.kiseleva@mail.mcgill.ca>
2022-04-10 07:55:45 +0000

    Test accessing body of inbound SMS

--- experiments/twilio-api/sms.js
+++ experiments/twilio-api/sms.js
@@ -10,42 +10,49 @@
 // and set the environment variables. See http://twil.io/secure
 
 
-const accountSid = "ACeadf543eb1764173a0fa8466747f885e";//process.env.TWILIO_ACCOUNT_SID;
-const authToken = "fbff4d0231a03b21a4575424599019d0"; process.env.TWILIO_AUTH_TOKEN;
+const accountSid = "ACeadf543eb1764173a0fa8466747f885e"; //process.env.TWILIO_ACCOUNT_SID;
+const authToken = "fbff4d0231a03b21a4575424599019d0"; // process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
 
+const sendMessageToClient = () => {
+  client.messages
+    .create({
+      body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+      from: '+18077709123', //process.env.MY_PHONE_NUMBER
+      to: '+15145855582'
+    })
+    .then(message => console.log(message.sid))
+    .catch((err) => console.log(err));
+}
 
-const messageClient = client.messages
-  .create({
-     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-     from: '+18077709123', //process.env.MY_PHONE_NUMBER
-     to: '+15145855582'
-   })
-  .then(message => console.log(message.sid))
-  .catch((err) => console.log(err));
 
-  //----Receiving requires a server!
+  //----Receiving requires a server, setting up a test server for now
 
   const http = require('http');
   const express = require('express');
   const MessagingResponse = require('twilio').twiml.MessagingResponse;
   
   const app = express();
+  app.use(express.urlencoded({ extended: true }));
 
   app.get("/", (req, res) => {
     res.send("The index works!");
   });
   
   app.post('/sms', (req, res) => {
+
+    //console.log(req.body.Body);
+    const numMinutes = Number(req.body.Body);
+    console.log(numMinutes, typeof(numMinutes));
+
+
     const twiml = new MessagingResponse();
-  
     twiml.message('The Robots are coming! Head for the hills!');
-  
     res.writeHead(200, {'Content-Type': 'text/xml'});
     res.end(twiml.toString());
-
     console.log("It happened!");
+
   });
 
 
