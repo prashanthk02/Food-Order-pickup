8fa931cb70f9cda8063b7a193f1556a76a93b099
Daria Kiseleva <daria.kiseleva@mail.mcgill.ca>
2022-04-10 08:24:34 +0000

    Make notes in README

--- experiments/twilio-api/sms.js
+++ experiments/twilio-api/sms.js
@@ -9,10 +9,24 @@
 // Find your Account SID and Auth Token at twilio.com/console
 // and set the environment variables. See http://twil.io/secure
 
+// #---------TWILIO
 
-const accountSid = "ACeadf543eb1764173a0fa8466747f885e"; //process.env.TWILIO_ACCOUNT_SID;
-const authToken = "fbff4d0231a03b21a4575424599019d0"; // process.env.TWILIO_AUTH_TOKEN;
+// # Twilio credentials
+// TWILIO_ACCOUNT_SID: "ACeadf543eb1764173a0fa8466747f885e"
+// TWILIO_AUTH_TOKEN: "fbff4d0231a03b21a4575424599019d0"
+
+// #Twilio key instead of auth tokens, not sure if will use this...
+// TWILIO_SID=SK7ce5ce1b11c54bd90f7c36856ae44cb3
+// TWILIO_SECRET=cLB85XqC3fJNYfEnP0tyfMJN9AMqO7I8
+
+require("dotenv").config(); ///! Unsure how to get it out of there, pasted neutrally
+
+// console.log(process.env.TWILIO_ACCOUNT_SID);
+
+const accountSid = "ACeadf543eb1764173a0fa8466747f885e";
+const authToken = "fbff4d0231a03b21a4575424599019d0";
 const client = require('twilio')(accountSid, authToken);
+
 
 
 const sendMessageToClient = () => {
